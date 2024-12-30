d7eebcfe36c2466bcacbe671269022bf59792d8f
Shagi <lazarevan333@gmail.com>
2024-12-30 09:48:22 +0500

    work

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,74 @@
 function getArrayParams(...arr) {
-
-  return { min: min, max: max, avg: avg };
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
+  for (let i = 0; i< arr.length; i++) {
+	  const num =arr[i];
+	  if (num > max) {
+		  max = num;
+	  }
+	  if (num < min) {
+		  min = num;
+	  }
+          sum +=num;
+  }
+  const avg = (sum / arr.length).toFixed(2);
+  return {
+	  min: min,
+	  max: max,
+	  avg: Numder(avg)
+  };
 }
-
 function summElementsWorker(...arr) {
-
+    if (arr.length === 0) return 0; 
+    return arr.reduce((sum, num) => sum + num, 0);
 }
 
 function differenceMaxMinWorker(...arr) {
-
+    if (arr.length === 0) return 0; 
+    const max = Math.max(...arr); 
+    const min = Math.min(...arr); 
+    return max - min; 
 }
 
 function differenceEvenOddWorker(...arr) {
+    if (arr.length === 0) return 0; 
+    let sumEvenElement = 0;
+    let sumOddElement = 0;
 
+    for (const num of arr) {
+        if (num % 2 === 0) {
+            sumEvenElement += num; 
+        } else {
+            sumOddElement += num; 
+        }
+    }
+    return sumEvenElement - sumOddElement; 
 }
 
 function averageEvenElementsWorker(...arr) {
+    if (arr.length === 0) return 0; 
+    let sumEvenElement = 0;
+    let countEvenElement = 0;
 
+    for (const num of arr) {
+        if (num % 2 === 0) {
+            sumEvenElement += num; 
+            countEvenElement++; 
+        }
+    }
+
+    return countEvenElement > 0 ? sumEvenElement / countEvenElement : 0; 
 }
+function makeWork(arrOfArr, func) {
+    let maxWorkerResult = -Infinity;
 
-function makeWork (arrOfArr, func) {
+    for (const arr of arrOfArr) {
+        const result = func(...arr); 
+        if (result > maxWorkerResult) { 
+            maxWorkerResult = result; 
+        }
+    }
 
+    return maxWorkerResult; 
 }
--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,53 @@
-﻿
+﻿function parseCount(value) {
+    const parsedValue = Number.parseFloat(value);
+    if (isNaN(parsedValue)) {
+        throw new Error('Невалидное значение');
+    }
+    return parsedValue;
+}
+
+function validateCount(value) {
+    try {
+        return parseCount(value);
+    } catch (error) {
+        return error;
+    }
+}
+
+
+class Triangle {
+    constructor(sideA, sideB, sideC) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+
+        if (sideA + sideB <= sideC || sideA + sideC <= sideB || sideB + sideC <= sideA) {
+            throw new Error('Треугольник с такими сторонами не существует');
+        }
+    }
+
+    get perimeter() {
+        return this.sideA + this.sideB + this.sideC;
+    }
+
+    get area() {
+        const s = this.perimeter / 2; 
+        const area = Math.sqrt(s * (s - this.sideA) * (s - this.sideB) * (s - this.sideC));
+        return parseFloat(area.toFixed(3)); 
+    }
+}
+
+function getTriangle(sideA, sideB, sideC) {
+    try {
+        return new Triangle(sideA, sideB, sideC);
+    } catch (error) {
+        return {
+            get area() {
+                return 'Ошибка! Треугольник не существует';
+            },
+            get perimeter() {
+                return 'Ошибка! Треугольник не существует';
+            }
+        };
+    }
+}
